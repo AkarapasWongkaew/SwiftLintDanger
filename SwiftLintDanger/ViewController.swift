2c84b3a7faec4bb7040197724e8b8f5d624ce57d
Akarapas Wongkaew <akarapaswongkaew@gmail.com>
2018-12-20 13:42:00 +0700

    Initial Commit

---
+++ SwiftLintDanger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftLintDanger
+//
+//  Created by Akarapas Wongkaew on 20/12/2561 BE.
+//  Copyright © 2561 Akarapas Wongkaew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
